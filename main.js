8ed3664734bf25354c73884ac64842c6622c5e0c
muhammadhammadnoman2008-sys <muhammadhammadnoman2008@gmail.com>
2025-10-17 04:37:50 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+let number=2
+if (number % 2 ===0) {
+    console.log("Even number.");
+}
+    else{
+        console.log("Odd number.");
+    }
+
+    let num=-7
+    if (num > 0){
+        console.log("The number is positive.");
+    }
+    else if(num < 0){
+        console.log("The number is negative.");
+    }
+    else{
+        console.log("The number is zero.");
+    }
+
+    let num1=76
+    let num2=98
+    if (num1 > num2){
+        console.log("num1 is larger than num2");
+    }
+    else if (num2 > num1){
+        console.log("num2 is larger than num1");
+    }
+    else{
+        console.log("num1 and num2 are equal");
+    }
+    
+    let age = 17;
+    age = parseInt(age);
+    if (age >= 18) {
+        console.log("Eligible is vote.");
+    } else {
+        console.log("Not Eligible.");
+    }
+
+    let marks=54
+    if (marks >=55) {
+        console.log("Pass");
+    }
+    else {
+        console.log("Fail");
+    }
+
